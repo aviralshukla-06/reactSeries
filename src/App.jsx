619e8f5ed988b28a59f91919167a3de87dcada67
aviralshukla-06 <aviralshukla733@gmail.com>
2025-01-12 20:27:38 +0530

    tried with useRef to store the value between re-renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const value = useRef;
+
+  function startClock() {
+    let currCount = setInterval(function () {
+      setTimer(c => c + 1)
+    }, 1000)
+    value.current = currCount;
+  }
+
+
+  function stopClock() {
+    clearInterval(value.current);
+  }
 
 
   return (
     <>
-      <h1>Jai HO!!!</h1>
+      {timer}
+      <br />
+
+      <button onClick={startClock}>Start</button>
+      <button onClick={stopClock}>Stop</button>
+
     </>
   )
 }
